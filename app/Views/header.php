2ef22a40b04deec482bc2be9f9910ebbf85ba54f
Erly123 <choquelayme@gmail.com>
2021-02-23 20:58:57 -0500

    actualizando

--- app/Views/header.php
+++ app/Views/header.php
@@ -224,6 +224,6 @@
 <i class="fab fa-facebook-square"></i><i class="fab fa-facebook-square"></i>
 
     <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-    
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit alias sapiente numquam similique consectetur libero, saepe at, obcaecati quia quos maxime voluptatibus, modi eligendi sed error eum rerum iste odio.</p>
 </body>
 </html> 
